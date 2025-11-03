ed50ee26c1ec6c6ac71962417e3af9e850b3711e
Aman Takria <amantakria93@gmail.com>
2025-11-04 01:14:58 +0530

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/color-palette"> 
+  <HashRouter>
     <App />
-  </BrowserRouter>
-)
+  </HashRouter>
+);
